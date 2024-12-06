24dfb903a911e14097f338069104ea0b5d2e50d3
Noppasin Sritongkul <noppasinsritongkul@NOP-MBP.local>
2024-12-06 12:21:49 +0700

    update

--- EKS/main.tf
+++ EKS/main.tf
@@ -2,52 +2,54 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-resource "aws_vpc" "internal-0-vpc" {
+resource "aws_vpc" "internal_vpc" {
   cidr_block           = "172.17.0.0/24"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    Name = "internal-0-vpc"
+    Name = "internal-vpc"
     Zone = "internal"
   }
 }
 
 resource "aws_subnet" "eks_subnet_1" {
-  vpc_id            = aws_vpc.internal-0-vpc.id
+  vpc_id            = aws_vpc.internal_vpc.id
   cidr_block        = "172.17.0.0/26"
   availability_zone = "ap-southeast-1a"
   tags = {
     Name = "eks-subnet-1"
     Zone = "internal"
-    VPC  = "internal-0-vpc"
+    VPC  = "internal-vpc"
   }
 }
 
 resource "aws_subnet" "eks_subnet_2" {
-  vpc_id            = aws_vpc.internal-0-vpc.id
+  vpc_id            = aws_vpc.internal_vpc.id
   cidr_block        = "172.17.0.64/26"
   availability_zone = "ap-southeast-1b"
   tags = {
     Name = "eks-subnet-2"
     Zone = "internal"
-    VPC  = "internal-0-vpc" 
+    VPC  = "internal-vpc"
   }
 }
 
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "eks-internal-0"
+  cluster_name    = "eks-int"
   cluster_version = "1.27"
-  vpc_id          = aws_vpc.internal-0-vpc.id
+  vpc_id          = aws_vpc.internal_vpc.id
   subnet_ids      = [aws_subnet.eks_subnet_1.id, aws_subnet.eks_subnet_2.id]
 
   eks_managed_node_groups = {
-    internal  = {
-      name          = "eks-node-group-internal-0"
+    internal = {
+      name          = "eks-ng-int"
       instance_type = "t3.medium"
       min_size      = 1
       desired_size  = 2
       max_size      = 3
     }
   }
+
+  iam_role_use_name_prefix = true
 }
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # assignment
+
+2. Build the image using the Dockerfile and push to Docker Hub
+
+```
+scarifcitadel/helloapi:v0.1.0 (ARM ARCH)
+
+scarifcitadel/helloapi:v0.2.0 (X86 ARCH)
+
+scarifcitadel/helloapi:v0.3.0 
+(example: curl 127.0.0.1:8080/helloapi?name=<STRING>) 
+ ```
